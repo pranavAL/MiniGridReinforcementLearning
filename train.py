fba67886ee42d6bbb08b6e09ec00cb53847796ef
pranavAL <pranav2109@hotmail.com>
2022-03-24 18:22:04 -0400

    Error

--- train.py
+++ train.py
@@ -20,11 +20,11 @@ models_dir = "models/PPO"
 os.makedirs(logdir, exist_ok=True)
 os.makedirs(models_dir, exist_ok=True)
 
-if not args.test:
-    model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=logdir)
-    model.learn(total_timesteps=300000)
-    model.save(f"{models_dir}/PPO")
-else:
+model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=logdir)
+model.learn(total_timesteps=300000)
+model.save(f"{models_dir}/PPO")
+
+if args.test:
     model = PPO.load(f"{models_dir}/PPO")
     num_episodes = 10
     for eps in range(num_episodes):
