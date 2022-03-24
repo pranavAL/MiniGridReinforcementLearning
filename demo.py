c1a3342edaca07749deeba36c4ff17d55b677acd
pranavAL <pranav2109@hotmail.com>
2022-03-24 08:37:54 -0400

    check

--- demo.py
+++ demo.py
@@ -9,13 +9,14 @@ import matplotlib.pyplot as plt
 from stable_baselines3.common.env_checker import check_env
 
 env = gym.make('MiniGrid-MultiCrossingKey-v1')
-check_env(env)
+#check_env(env)
 
 print(f"Observation Space: {env.observation_space}")
 print(f"Shape: {env.observation_space.shape}")
 print(f"Action space: {env.action_space}")
 
 obs = env.reset()
+print(type(obs))
 img = env.render('rgb_array')
 action = env.action_space.sample()
 print(f"Sampled action: {action}")
