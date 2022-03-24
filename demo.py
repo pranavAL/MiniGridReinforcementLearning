3d77d351e289d69995d00ef3ab69813c2f636ccc
Pranav Agarwal <pranav2109@hotmail.com>
2022-03-24 07:00:52 -0400

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import time
+import argparse
+import numpy as np
+import gym
+import gym_minigrid
+from gym_minigrid.wrappers import *
+import matplotlib.pyplot as plt
+
+env = gym.make('MiniGrid-MultiCrossingKey-v1')
+
+print(f"Observation Space: {env.observation_space}")
+print(f"Shape: {env.observation_space.shape}")
+print(f"Action space: {env.action_space}")
+
+obs = env.reset()
+img = env.render('rgb_array')
+action = env.action_space.sample()
+print(f"Sampled action: {action}")
+obs, reward, done, info = env.step(action)
+plt.imshow(obs)
+plt.show()
+
+print(obs.shape, reward, done, info)
---
+++ train.py
@@ -0,0 +1,26 @@
+import numpy as np
+import gym
+import gym_minigrid
+from gym_minigrid.wrappers import *
+from gym_minigrid.window import Window
+
+from stable_baselines3 import DQN
+
+env = gym.make('MiniGrid-MultiCrossingKey-v1')
+
+model = DQN("CnnPolicy", env, verbose=1)
+model.learn(total_timesteps=10000)
+num_episodes = 200
+
+for eps in range(num_episodes):
+    current_reward = []
+    obs = env.reset()
+    for i in range(1000):
+        action, _states = model.predict(obs, deterministic=True)
+        obs, rewards, dones, info = env.step(action)
+        current_reward.append(rewards)
+        env.render()
+        if dones:
+            obs = env.reset()
+    print(f"Episode: {eps},  Average reward: {np.mean(current_reward)}")
+env.close()
